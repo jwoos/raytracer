1fb6f315af20e3aebdf00adf947ddef86b87e31b
Jun Woo Shin <shin.junwoo97@gmail.com>
2020-08-17 20:13:55 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let width = 256;
+    let height = 256;
+
+    print!("P3\n{} {}\n255\n", width, height);
+
+    for i in 0..width {
+        for j in 0..height {
+            let r = i;
+            let b = 64;
+            let g = j;
+
+            print!("{} {} {}\n", r, g, b);
+        }
+    }
+}
